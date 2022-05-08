e7f59dc872b6cc3142a534d6df3d9c81911bd773
PeleBahalag <102541131+PeleBahalag@users.noreply.github.com>
2022-05-08 17:18:19 +0300

    fullscreen fix

--- script.js
+++ script.js
@@ -21,12 +21,11 @@ const startVid = () => {
 }
 
 
+
 // ---------------------
 // Add components at the end of the video (listener)
 const vidEnded = (event) => {
-    if (document.fullscreenElement) {
-        setTimeout(() => document.exitFullscreen(), 10);
-    }
+    video.webkitExitFullscreen();
     if (source.src.includes("Intro")) {
         document.getElementById("btn-container").style.display = "flex";
         document.getElementById("restart").style.display = "block";
